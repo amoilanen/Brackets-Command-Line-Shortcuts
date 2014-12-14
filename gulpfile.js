744374d012c8a6bae95b7b60767578b2dffc9854
anton <anton.al.ivanov@gmail.com>
2014-12-14 22:12:05 +0200

    Enabled running Mocha tests with Should asserts

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
-var jshint = require("gulp-jshint");
+var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
 
 gulp.task('lint', function(cb) {
-  gulp.src("./src/*.js")
+  gulp.src('./src/*.js')
     .pipe(jshint())
-    .pipe(jshint.reporter("default"));
+    .pipe(jshint.reporter('default'));
 });
 
-gulp.task('default', ['lint']);
+gulp.task('test', function(cb) {
+  gulp.src('spec/*.spec.js', {read: false})
+    .pipe(mocha({reporter: 'spec'}));
+});
+
+gulp.task('default', ['lint', 'test']);
---
+++ spec/Configuration.spec.js
@@ -0,0 +1,9 @@
+var assert = require('should')
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      [1, 2, 3].indexOf(5).should.be.exactly(-1);
+    });
+  });
+});
