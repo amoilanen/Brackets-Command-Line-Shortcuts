c4e17be03eb3ba21f69d4578d7f34a3bdf60b51e
Anton Ivanov <anton.al.ivanov@gmail.com>
2014-12-18 06:23:48 +0200

    Added more tests for CommandLine

--- spec/CommandLine.spec.js
+++ spec/CommandLine.spec.js
@@ -9,6 +9,15 @@ describe('CommandLine', function() {
   var commandLine;
   var connectionListenStub;
 
+  function handler1() {
+  }
+
+  function handler2() {
+  }
+
+  function handler3() {
+  }
+
   beforeEach(function() {
     commandLine = new CommandLine();
     connectionListenStub = sinon.stub(commandLine, '_connectionListen');
@@ -19,13 +28,6 @@ describe('CommandLine', function() {
   });
 
   it('should register multiple listeners properly', function() {
-    function handler1() {
-    }
-    function handler2() {
-    }
-    function handler3() {
-    }
-
     commandLine.addListeners({
       'event1': handler1,
       'event2': handler2,
@@ -36,5 +38,42 @@ describe('CommandLine', function() {
       ['extension.commandline.node.event2', handler2],
       ['extension.commandline.node.event3', handler3]
     ]);
+  });
+
+  it('should not register any listeners if handlers is an empty object', function() {
+    commandLine.addListeners({});
+
+    connectionListenStub.getCalls().length.should.be.equal(0);
+  });
+
+  it('should not register any listeners if handlers is an empty array', function() {
+    commandLine.addListeners([]);
+
+    connectionListenStub.getCalls().length.should.be.equal(0);
+  });
+
+  it('should register all listeners when called in sequence', function() {
+    commandLine.addListeners({
+      'event1': handler1
+    });
+    commandLine.addListeners({
+      'event2': handler2
+    });
+    commandLine.addListeners({
+      'event3': handler3
+    });
+
+    connectionListenStub.getCalls().map(function(call) { return call.args; }).should.be.eql([
+      ['extension.commandline.node.event1', handler1],
+      ['extension.commandline.node.event2', handler2],
+      ['extension.commandline.node.event3', handler3]
+    ]);
+  });
+
+  it('should not register any listeners if handlers is null or undefined', function() {
+    commandLine.addListeners(null);
+    commandLine.addListeners(undefined);
+
+    connectionListenStub.getCalls().length.should.be.equal(0);
   });
 });
